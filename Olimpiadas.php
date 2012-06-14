5117bdb619f73206ae98f5c3d33035249cc49ea0
Gonzalo Ayuso <gonzalo123@gmail.com>
2012-06-14 14:57:03 +0200

    naming

--- Olimpiadas.php
+++ Olimpiadas.php
@@ -1,7 +1,7 @@
 <?php
 class Olimpiadas
 {
-    public function getPossition($pos)
+    public function getPosition($pos)
     {
         return substr(implode(null, range(1, $pos)), $pos-1, 1);
     }
--- tests/OlimpiadasTest.php
+++ tests/OlimpiadasTest.php
@@ -6,10 +6,10 @@ class OlimpiadasTest extends PHPUnit_Framework_TestCase
     public function testSimple()
     {
         $olimpiadas = new Olimpiadas();
-        $this->assertEquals(1, $olimpiadas->getPossition(1));
-        $this->assertEquals(2, $olimpiadas->getPossition(2));
-        $this->assertEquals(1, $olimpiadas->getPossition(10));
-        $this->assertEquals(0, $olimpiadas->getPossition(11));
-        $this->assertEquals(7, $olimpiadas->getPossition(206788));
+        $this->assertEquals(1, $olimpiadas->getPosition(1));
+        $this->assertEquals(2, $olimpiadas->getPosition(2));
+        $this->assertEquals(1, $olimpiadas->getPosition(10));
+        $this->assertEquals(0, $olimpiadas->getPosition(11));
+        $this->assertEquals(7, $olimpiadas->getPosition(206788));
     }
 }
