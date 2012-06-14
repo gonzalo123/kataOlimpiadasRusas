1916595d64a4d25ed8dc2d1dc5b8e899796bf23e
Gonzalo Ayuso <gonzalo123@gmail.com>
2012-06-14 17:23:37 +0200

    optimizando el consumo de memoria

--- Olimpiadas.php
+++ Olimpiadas.php
@@ -3,6 +3,12 @@ class Olimpiadas
 {
     public function getPosition($pos)
     {
-        return substr(implode(null, range(1, $pos)), $pos-1, 1);
+        return substr(implode(null, range(1, $this->getMaxNumber($pos))), $pos-1, 1);
+    }
+
+    private function getMaxNumber($pos)
+    {
+        $x = (integer) str_repeat(9, strlen((string) $pos)-1);
+        return floor($x + ($pos - $x) / 2) + 1;
     }
 }
